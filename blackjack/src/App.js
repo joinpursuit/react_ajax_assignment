3d589ac959cf1d4e6a643b87ce9c36028a20f1c6
Andrew KIl <kil.andrew@gmail.com>
2019-01-15 17:19:28 -0500

    works...! but not finished

--- blackjack/src/App.js
+++ blackjack/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
+import Cards from "./Cards.js";
 import "./App.css";
 
 class App extends Component {
@@ -7,46 +8,112 @@ class App extends Component {
     super();
 
     this.state = {
-      deckURL: "",
-      firstCard: "",
-      secondCard: ""
+      firstPage: true,
+      deckID: "",
+      cards: []
     };
   }
-  getNewDeck = () => {
+  newDeck = () => {
+    let cardState = [...this.state.cards];
     axios
       .get("https://deckofcardsapi.com/api/deck/new/draw/?count=2")
       .then(response => {
-        debugger;
-
+        cardState.push(
+          response.data.cards[0].image,
+          response.data.cards[1].image
+        );
         this.setState({
-          deckURL: response.data.deck_id,
-          firstCard: response.data.cards[0].image,
-          secondCard: response.data.cards[1].image
+          deckID: response.data.deck_id,
+          cards: cardState
         });
-
-        console.log(`Hurray! Your deck ID is ${this.state.deckURL}`);
+        console.log(cardState);
+        console.log(`Hurray! Your deck ID is ${this.state.deckID}`);
+        console.log(this.state.cards);
       })
       .catch(error => {
-        console.log("Error: Unable to get new deck");
+        console.log("Error");
       });
+    this.setState({
+      firstPage: false
+    });
+  };
+
+  existingDeck = () => {
+    console.log(this.state.deckID);
+    let cardState = [...this.state.cards];
+    axios
+      .get(
+        `https://deckofcardsapi.com/api/deck/${this.state.deckID}/draw/?count=2`
+      )
+      .then(response => {
+        cardState.push(
+          response.data.cards[0].image,
+          response.data.cards[1].image
+        );
+        this.setState({
+          deckID: response.data.deck_id,
+          cards: cardState
+        });
+      })
+      .catch(error => {
+        console.log("Error");
+      });
+    this.setState({
+      firstPage: false
+    });
+  };
+
+  updateInputValue = event => {
+    this.setState({
+      deckID: event.target.value
+    });
+  };
+
+  drawCard = () => {
+    let cardState = [...this.state.cards];
+    axios
+      .get(
+        `https://deckofcardsapi.com/api/deck/${this.state.deckID}/draw/?count=1`
+      )
+      .then(response => {
+        cardState.push(response.data.cards[0].image);
+        this.setState({
+          cards: cardState
+        });
+      })
+      .catch(error => {
+        console.log("Error");
+      });
+    console.log(cardState);
+    console.log(this.state.deckID);
   };
 
   render() {
-    const { firstCard, secondCard } = this.state;
-    return (
-      <div className="App">
-        <h1>Blackjack</h1>
-        <button onClick={this.getNewDeck}>Generate Deck</button>
-        <p>Input Existing Deck</p>
-        <input type="text" />
-
-        <div>
-          <br />
-          <img className="firstCard" alt="" src={firstCard} />
-          <img className="secondCard" alt="" src={secondCard} />
+    const { firstPage, deckID, cards } = this.state;
+    if (firstPage === true) {
+      return (
+        <div className="App">
+          <h1>Blackjack</h1>
+          <button onClick={this.newDeck}>Generate New Deck</button>
+          <p>Input Existing Deck</p>
+          <input type="text" value={deckID} onChange={this.updateInputValue} />
+          <button onClick={this.existingDeck}>Generate Existing Deck</button>
         </div>
-      </div>
-    );
+      );
+    } else {
+      return (
+        <div className="App">
+          <h1>Blackjack</h1>
+          <p>Deck ID: {deckID}</p>
+          <br />
+          {cards.map(image => (
+            <Cards source={image} key={image} />
+          ))}
+          <br />
+          <button onClick={this.drawCard}>Hit me!</button>
+        </div>
+      );
+    }
   }
 }
 
