814f703ee17ba1a2fcf2efc5f8da91c34049bb1d
Michell Tejada <michelltejada@pursuit.com>
2019-01-25 15:11:12 -0500

    Mostly done

--- blackjack/src/App.js
+++ blackjack/src/App.js
@@ -1,113 +1,51 @@
 import React, { Component } from 'react';
 import './App.css';
 import axios from 'axios'
-// import Menu  from './menu.js'
-// import Hand from './hand.js'
+import Menu  from './Menu.js'
+import Hand from './Hand.js'
+
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      // playerHand: [],
       deckId: '',
-      drawCard: '',
-      currentDeck: []
+      deck: []
     }
   }
 
-  handleDraw = (event) => {
-    this.setState({
-      [event.target.name]: event.target.value
-    })
-  }
+  makeDeck = async () => {
+     let response = await axios.get(
+       "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1"
+     );
+     let newDeckId = response.data.deck_id;
+     this.setState({ deckId: newDeckId });
+     let response2 = await axios.get(`https://deckofcardsapi.com/api/deck/${newDeckId}/draw/?count=2`);
 
-  handleCardDeck = () => {
-    axios
-      .get('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
-      .then(response => {
-        this.setState({
-          deckId: response.data.deckId
-        })
-      })
-      .catch(error => {
-        console.log(error);
-      })
-  }
+     let currentHand = response2.data.cards //object here
+     this.setState({ deck: currentHand })
 
-  handleDrawCard = () => {
-    axios
-      .get('https://deckofcardsapi.com/api/deck/<<deck_id>>/draw/?count=2')
-      .then(response => {
-        let cardData = response.data.drawCard
-        let newDeck = this.state.currentDeck;
-        for(let i = 0; i < cardData.length; i++) {
-          newDeck.push(cardData[i].image)
-        }
-        this.setState({
-          drawCard: cardData,
-          currentDeck: newDeck
-        })
-      })
-      .catch(error => {
-        console.log(error);
-      })
-  }
+   };
 
-  displayCurrentDeckOfCards = () => {
-    let currentDeck = this.state.currentDeck;
-    return currentDeck.map(url => {
-      return <img alt='cards' src={url}></img>
-    })
-  }
+   getCard = async () => {
+     const { deckId, deck } = this.state;
+
+     let response = await axios.get(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`)
+
+     let nextCard = response.data.cards;
+     this.setState({ deck: [...deck, ...nextCard] })
+   }
 
 
-
-  render() {
-
-    const { deckId, drawCard } = this.state
-
-    if(!drawCard) {
-      return (
-        <div>
-          <h1>Blackjack</h1>
-          <button onClick={this.handleCardDeck}>Generate Deck</button>
-          <input type='text' name='deckId' value={deckId} placeholder='Type existing Id' onChange={this.handleDraw} />
-          <button onClick={this.handleDrawCard}>Draw!</button>
-        </div>
-      )
-    } else {
-      return (
-        <div>
-          <h1>Blackjack</h1>
-          <p> Deck id: {deckId}</p>
-          <div>
-          
-            {this.displayCurrentDeckOfCards()}
-            <button onClick={this.handleDrawCard}>Hit Me!</button>
-
-            </div>
-        </div>
-      )
-    }
-  }
+   render(){
+     const { deckId, deck } = this.state
+     return(
+       <div>
+        <Menu newDeck={this.makeDeck} deckId={deckId} />
+        <Hand deck={deck} draw={this.getCard} />
+       </div>
+     )
+   }
 }
 
 export default App;
-
-
-
-// const { gameStart } = this.state
-// if(gameStart) {
-// return(
-//     <div>
-//       <Hand />
-//     </div>
-//   )
-//
-// } else {
-//   return (
-//     <div>
-//       <Menu deckId={this.state.deckId} playerHand={this.state.playerHand} handleDraw={this.handleDraw} />
-//     </div>
-//   )
-// }
